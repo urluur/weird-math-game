5dbbdfa53568b4d31adf8041da44b3cba43b8bdb
Luka Uršič <urlu@pm.me>
2022-12-29 19:14:46 +0100

    Fixed layout

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.util.Random;
 
@@ -7,30 +8,40 @@ public class Gui {
     private JPanel buttonsPanel, topPanel;
     private JLabel targetValue, currentSum, movesLeft;
 
+    private Border debugBorder = BorderFactory.createLineBorder(Color.GREEN, 3);
+    private Border defaultBorder = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+
     Gui() {
         // frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("More or less, less is more!");
         frame.setSize(800, 600);
+        frame.setResizable(false);
 
         // panel
         buttonsPanel = new JPanel();
-        buttonsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        buttonsPanel.setBorder(defaultBorder);
         buttonsPanel.setLayout(new GridLayout(9,9));
 
         // top panel
         topPanel = new JPanel(new GridLayout(0,3));
-        topPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        topPanel.setBorder(defaultBorder);
 
         addButtons();
 
         targetValue = new JLabel("Target value:");
+        targetValue.setHorizontalAlignment(JLabel.CENTER);
+        topPanel.add(new JLabel());
         topPanel.add(targetValue, BorderLayout.CENTER);
 
         currentSum = new JLabel("Current sum:");
+        currentSum.setVerticalAlignment(JLabel.CENTER);
+        currentSum.setBorder(defaultBorder);
+        currentSum.setHorizontalAlignment(JLabel.CENTER);
         frame.add(currentSum, BorderLayout.SOUTH);
 
         movesLeft = new JLabel("Moves left:");
+        movesLeft.setHorizontalAlignment(JLabel.RIGHT);
         topPanel.add(movesLeft, BorderLayout.EAST);
 
         frame.add(topPanel, BorderLayout.NORTH);
